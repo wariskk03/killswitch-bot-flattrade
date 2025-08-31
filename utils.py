6a059f3cf40e022b2794dfd11b4b31cc288d1e37
Waris Khan <wariskk03@gmail.com>
2025-09-01 03:40:42 +0530

    feat: implement killswitch automation with Selenium
    - Added Selenium, WebDriverWait, pyotp for automating login & killswitch process
    - Implemented login flow with credentials from cred.json
    - Automated navigation to "Kill Switch" tab
    - Selects relevant checkboxes and clicks deactivate
    - Keeps browser window alive until manually closed

--- utils.py
+++ utils.py
@@ -1,4 +1,10 @@
 from NorenRestApiPy.NorenApi import NorenApi
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+import pyotp
 import json
 
 def session():
@@ -73,4 +79,51 @@ def flatten(open_positions, open_orders, api):
     cancel_orders(open_orders, api)
 
 def killswitch():
-    pass
+    with open("/Users/wariskhan/Python Algo/flattrade_api/killswitch_tool/cred.json") as f:
+        cred = json.load(f)
+        user, pas, totp_token = cred["user_id"], cred["pass"], cred["totp_token"]
+
+    driver = webdriver.Chrome()
+    driver.minimize_window()
+
+    driver.get("https://auth.flattrade.in/?app=wall")
+
+    username = driver.find_element(By.ID, "input-19")
+    username.send_keys(user)
+
+    password = driver.find_element(By.ID, "pwd")
+    password.send_keys(pas)
+
+    totp_code = pyotp.TOTP(totp_token).now()
+    totp = driver.find_element(By.ID, "pan")
+    totp.send_keys(totp_code)
+
+    login_btn = driver.find_element(By.ID, "sbmt")
+    login_btn.click()
+
+    my_account = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//a[3]//span[1]")))
+    my_account.click()
+
+    time.sleep(0.2)
+    WebDriverWait(driver, 10).until(EC.invisibility_of_element_located((By.CSS_SELECTOR, ".v-overlay__scrim")))
+
+    killswitch_tab = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//b[normalize-space(text())='Kill Switch']")))
+    killswitch_tab.click()
+
+    checkboxes = WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.CLASS_NAME, "v-input--selection-controls__ripple")))
+
+    checkboxes[1].click()
+    checkboxes[6].click()
+
+    deactivate = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button[class='v-btn v-btn--has-bg theme--light elevation-0 v-size--small primary'] span[class='v-btn__content']")))
+    deactivate.click()
+    driver.maximize_window()
+
+    try:
+        while True:
+            time.sleep(1)
+            driver.title
+    except:
+        print("Browser is closed")
+
+killswitch()
